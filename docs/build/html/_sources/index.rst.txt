834c1fce0b87fa52e9c71d103d62aa3563b6593e
suqik <suchen@shao.ac.cn>
2024-07-11 14:03:54 +0800

    add a basic framework

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+.. BEGINNING documentation master file, created by
+   sphinx-quickstart on Thu Jul 11 13:32:16 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+*********************************************************************
+BEGINNING: Basics and Essentials for Getting INto astroNomy computING
+*********************************************************************
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Tips and Notations
+
+   c_cpp
+   shell
+   mpi
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Useful Tools Introduction
+
+   LSF
+   Slurm
---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'BEGINNING'
+copyright = '2024, Su and Yang'
+author = 'Su and Yang'
+release = '0.1'
+version = 'v0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
